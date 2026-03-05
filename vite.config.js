7c8713314076ec9ed79cd9f4fce8c27822736446
dmartinez <dmartinez@solucionesamericana.com.co>
2026-03-05 07:03:37 -0500

    feat:(deploy) add yaml for github actions

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/portafolio-vue/',
+  base: '/portfolio-vue/',
   plugins: [vue()],
    resolve: {
     alias: {
